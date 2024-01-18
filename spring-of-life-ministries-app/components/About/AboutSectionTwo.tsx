fedfbb7e7a254abb722e90cf33aad82a7697d502
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-01-18 11:55:47 +0100

    Improved the UI/UX of the pages

--- spring-of-life-ministries-app/components/About/AboutSectionTwo.tsx
+++ spring-of-life-ministries-app/components/About/AboutSectionTwo.tsx
@@ -121,10 +121,10 @@ const AboutSectionTwo = () => {
 
     <section className="bg-gray-700 bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-center bg-no-repeat bg-blend-multiply">
       <div className="mx-auto max-w-screen-xl px-4 py-24 text-center lg:py-56">
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+        <h1 className="mt-24 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
           We invest in the world’s potential
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
+        <p className="mt text-lg font-normal text-gray-300 sm:px-16 lg:px-48 lg:text-xl">
           Here at Flowbite we focus on markets where technology, innovation, and
           capital can unlock long-term value and drive economic growth.
         </p>
